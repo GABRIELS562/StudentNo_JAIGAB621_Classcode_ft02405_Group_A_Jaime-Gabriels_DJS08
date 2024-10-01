00ff2165f04e41ecf4f4820e72164d7cb85654bb
Gabriels562 <63159064+GABRIELS562@users.noreply.github.com>
2024-10-01 21:22:53 +0200

    chore npm install mirage, added van detail

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import Vans from "./pages/Vans"
+import Vans from ".pages/Vans"
 import VanDetail from "./pages/VanDetail"
 
 import "./server"
@@ -11,13 +11,6 @@ import "./server"
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
