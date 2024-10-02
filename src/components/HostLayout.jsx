a3de5a3c08ca372ff8aa593e95bfcb1cf8ffe976
Gabriels562 <63159064+GABRIELS562@users.noreply.github.com>
2024-10-02 10:42:10 +0200

    Chore: Completed code. Prelim submission

--- src/components/HostLayout.jsx
+++ src/components/HostLayout.jsx
@@ -1,53 +1,48 @@
 import React from "react"
-import { NavLink, Outlet } from "react-router-dom"
+import { Link, NavLink } from "react-router-dom"
+import imageUrl from "/assets/images/avatar-icon.png"
 
-export default function HostLayout() {
-    /**
-     * Challenge - part 2:
-     * Make the host navbar indicate the currently-active route.
-     * 
-     * Use the following CSS rules:
-     *      font-weight: bold;
-     *      text-decoration: underline;
-     *      color: #161616;
-     * 
-     * I'd recommend using an inline style this time.
-     * 
-     * NOTE: There will be a small bug that we'll fix 
-     * after you do the challenge.
-     */
+export default function Header() {
     const activeStyles = {
         fontWeight: "bold",
         textDecoration: "underline",
         color: "#161616"
     }
 
+    function fakeLogOut() {
+        localStorage.removeItem("loggedin")
+    }
+
     return (
-        <>
-            <nav className="host-nav">
+        <header>
+            <Link className="site-logo" to="/">#VanLife</Link>
+            <nav>
                 <NavLink
                     to="/host"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
-                    Dashboard
+                    Host
                 </NavLink>
-
                 <NavLink
-                    to="/host/income"
+                    to="/about"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
-                    Income
+                    About
                 </NavLink>
-
                 <NavLink
-                    to="/host/reviews"
+                    to="/vans"
                     style={({ isActive }) => isActive ? activeStyles : null}
                 >
-                    Reviews
+                    Vans
                 </NavLink>
-
+                <Link to="login" className="login-link">
+                    <img
+                        src={imageUrl}
+                        className="login-icon"
+                    />
+                </Link>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
-            <Outlet />
-        </>
+        </header>
     )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,13 +2,6 @@ import React from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import { loginUser } from "../api"
 
-/**
- * Challenge: read up on the useNavigate hook from the
- * docs and implement it in the VanLife app. When the user
- * successfully logs in, they should be redirected to the 
- * /host route.
- */
-
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
     const [status, setStatus] = React.useState("idle")
@@ -17,13 +10,16 @@ export default function Login() {
     const location = useLocation()
     const navigate = useNavigate()
 
+    const from = location.state?.from || "/host";
+
     function handleSubmit(e) {
         e.preventDefault()
         setStatus("submitting")
         loginUser(loginFormData)
             .then(data => {
                 setError(null)
-                navigate("/host")
+                localStorage.setItem("loggedin", true)
+                navigate(from, { replace: true })
             })
             .catch(err => {
                 setError(err)
@@ -44,12 +40,12 @@ export default function Login() {
     return (
         <div className="login-container">
             {
-                location.state ?.message &&
+                location.state?.message &&
                     <h3 className="login-error">{location.state.message}</h3>
             }
             <h1>Sign in to your account</h1>
             {
-                error ?.message &&
+                error?.message &&
                     <h3 className="login-error">{error.message}</h3>
             }
 
