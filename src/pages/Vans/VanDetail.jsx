900a556cc625da1a681597cd2430f0b20378cc29
Gabriels562 <63159064+GABRIELS562@users.noreply.github.com>
2024-10-02 09:58:33 +0200

    chore: Added Not Found page

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -14,8 +14,10 @@ export default function VanDetail() {
             .then(data => setVan(data.vans))
     }, [params.id])
 
-  
+    
+    
     const search = location.state?.search || ""
+    const type = location.state?.type || "all"
     
     return (
         <div className="van-detail-container">
@@ -23,7 +25,7 @@ export default function VanDetail() {
                 to={`..${search}`}
                 relative="path"
                 className="back-button"
-            >&larr; <span>Back to all vans</span></Link>
+            >&larr; <span>Back to {type} vans</span></Link>
             
             {van ? (
                 <div className="van-detail">
